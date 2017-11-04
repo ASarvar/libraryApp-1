2a29b73c009fee97b58eaa53b056672f2ffe229e
Sarvar Alibekov <sarvar.bakhtiyor@gmail.com>
2017-11-05 01:59:06 +0500

    add 3 line

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -38,6 +38,7 @@
 		        <td>{{$user->role['name']}}</td>
 				add this line
 				add this line
+				add this line
 		        <td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
 		        <td>{{$user->created_at->diffForHumans()}}</td>
 		        <td>{{$user->updated_at->diffForHumans()}}</td>
