2666d133e2a1693f22747280f5943f67a889f4a8
Sarvar Alibekov <sarvar.bakhtiyor@gmail.com>
2017-11-05 01:53:54 +0500

    add line

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -36,6 +36,7 @@
 		        <td><a href="{{route('admin.users.edit', $user->id)}}">{{$user->name}}</a></td>
 		        <td>{{$user->email}}</td>
 		        <td>{{$user->role['name']}}</td>
+				add this line
 		        <td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
 		        <td>{{$user->created_at->diffForHumans()}}</td>
 		        <td>{{$user->updated_at->diffForHumans()}}</td>
